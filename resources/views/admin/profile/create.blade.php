7f7e21517890ba190e20d0fc956dc08de8017023
Shota Togawa <s.togawa0112@gmail.com>
2019-02-17 21:23:21 +0900

    finished  php_laravel12

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,1 +1,17 @@
-<h1>Profileのcreateページ</h1>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'My profile')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
